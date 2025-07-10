188b4265c731afa0cf4b9bcf0188a08520a43bbd
Tair Olegovich <gidrolaz.tr@gmail.com>
2025-07-10 13:09:54 +0700

    Intermediate results of the calculator presentation

---
+++ Calculator/Calculator/ICalcData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal interface ICalcData
+    {
+        // int - num of operation, char - symbol of operation 
+        Dictionary<int, char> Operations { get; set; }
+        // int - num of operation, double - numbers 
+        Dictionary<int, double> Values { get; set; }
+    }
+}
---
+++ Calculator/Calculator/IParserExpression.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal interface IParserExpression
+    {
+        void SetExp(string value);
+        string ParseExp();
+    }
+}
